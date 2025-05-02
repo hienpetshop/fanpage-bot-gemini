3fae8f4336c2c5d3abbc6204017fa85fa4ce56bb
hienpetshop <locpetshop@gmail.com>
2025-05-03 00:19:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post("/webhook", async (req, res) => {
             promptParts.push({
               text: `Bạn là người bán hàng online của fanpage Lộc Pet Shop. Trả lời tự nhiên, giống nhân viên tư vấn trên Facebook. Văn phong ngắn gọn, dễ hiểu, như đang nhắn tin với khách.
 Tập trung đúng thông tin khách cần, không dài dòng, không nói thừa.
-Không dùng văn vở lịch sự quá mức. Tránh kiểu “chào bạn”, “cảm ơn”, v.v.
+Không dùng văn vở lịch sự quá mức. Tránh kiểu “chào bạn”, “cảm ơn”, "nhé", "nha" v.v.
 Dưới đây là thông tin nội bộ cửa hàng:
 ${noidung_txt}
 
